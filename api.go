354416f66aad22e5159a80960225790620ab58c1
Shammah Chancellor <shammah.chancellor@gmail.com>
2023-03-11 15:15:19 -0800

    More wip

--- api.go
+++ api.go
@@ -3,32 +3,33 @@ package supergood
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
+const FLUSH_INTERVAL = 10
+
 type Interceptor struct {
-	core http.RoundTripper
+	transport    http.RoundTripper
+	requestMutex sync.Mutex
+	requests     []*http.Request
 }
 
-func (Interceptor) logRequest(r *http.Request) {
+func (*Interceptor) logRequest(r *http.Request) {
 	fmt.Println("Supergood Request:", r.Body)
 }
 
-func (Interceptor) logResponse(r *http.Response) {
+func (*Interceptor) logResponse(r *http.Response) {
 	fmt.Println("Supergood Response:", r.Body)
 }
 
-func (i Interceptor) RoundTrip(r *http.Request) (*http.Response, error) {
+func (i *Interceptor) RoundTrip(r *http.Request) (*http.Response, error) {
 	i.logRequest(r)
-	resp, err := i.core.RoundTrip(r)
+	resp, err := i.transport.RoundTrip(r)
 	i.logResponse(resp)
 	return resp, err
 }
 
 // Setup instruments the default client to log requests with the supergood API
-func Setup() {
-	// responseCache := cache.New(cache.NoExpiration, cache.NoExpiration)
-	// requestCache := cache.New(cache.NoExpiration, cache.NoExpiration)
-	http.DefaultClient = &http.Client{
-		Transport: Interceptor{http.DefaultTransport},
-	}
+func SetupClient(client *http.Client) {
+	client.Transport = &Interceptor{transport: client.Transport}
 }
--- api_test.go
+++ api_test.go
@@ -1,10 +1,28 @@
 package supergood
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 // TestHelloEmpty calls greetings.Hello with an empty string,
 // checking for an error.
-func TestHello(t *testing.T) {
+func TestInterceptor(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%+v\n", r)
+		t.Log("Hello")
+		w.Write([]byte(`{"isItSunday": true}`))
+	})))
+	defer srv.Close()
+
+	c := srv.Client()
+	SetupClient(c)
+
+	_, err := c.Get(srv.URL)
+	if err != nil {
+		t.Logf("%+v", err)
+		t.Fail()
+	}
 }
