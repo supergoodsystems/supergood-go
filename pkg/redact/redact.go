413ff98eec7fca5f7f50dfe0f5f0381af539b9ec
zbenamram <55831041+zbenamram@users.noreply.github.com>
2024-04-15 20:16:30 -0700

    [Feature]: ForceRedactAll - allow keys (#32)
    * Checking whether reflected value is zero value in prepareOutput
    
    * Redact all by default - allow keys to not be redacted
    
    * fix tests
    
    * adding tests
    
    * adding support for arrays
    
    * .
    
    * update tests

--- pkg/redact/redact.go
+++ pkg/redact/redact.go
@@ -15,23 +15,22 @@ func Redact(events []*event.Event, rc *remoteconfig.RemoteConfig) []error {
 	forceRedact := rc.IsRedactAllEnabled()
 	for _, e := range events {
 		domain := domainutils.GetDomainFromHost(e.Request.URL)
+		endpoints := rc.Get(domain)
+		if len(endpoints) == 0 && !forceRedact {
+			continue
+		}
+		endpoint := endpoints[e.MetaData.EndpointId]
 		if forceRedact {
-			meta, redactErrs := redactAll(domain, e.Request.URL, e)
+			meta, redactErrs := redactAll(domain, e, endpoint.SensitiveKeys)
 			errs = append(errs, redactErrs...)
 			e.MetaData.SensitiveKeys = append(e.MetaData.SensitiveKeys, meta...)
 			continue
 		}
 
-		endpoints := rc.Get(domain)
-		if len(endpoints) == 0 {
-			continue
-		}
-		endpoint, ok := endpoints[e.MetaData.EndpointId]
-		if !ok {
-			continue
-		}
-
 		for _, sensitiveKey := range endpoint.SensitiveKeys {
+			if sensitiveKey.Action != "REDACT" {
+				continue
+			}
 			formattedParts, err := formatSensitiveKey(sensitiveKey.KeyPath)
 			if err != nil {
 				errs = append(errs, err)
